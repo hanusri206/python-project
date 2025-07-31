feae68952bf58aa4192d83b10ebb385e2ad6b1b0
hanusri206 <kalakanishka66@gmail.com>
2025-07-31 15:55:02 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,11 @@
+num=int(input("enter a number"))
+if(num>=90):
+  print("grade A")
+elif(num>=80):
+  print("grade B")
+elif(num>=70):
+  print("grade C")
+elif(num>=65):
+  print("grade D")
+elif(num>=60):
+  print("grade D")
