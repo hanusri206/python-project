cbe73b18a00451695723a5f31dd4ce532dbbae14
hanusri206 <kalakanishka66@gmail.com>
2025-07-31 14:35:56 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,6 @@
+arr[1,"g",[2,"hi"],{"name",:"sethu","dept",one":}]
+                    print(arr[2][1])
+                    print(arr[2][1])
+                    print(arr[2][1][1])
+                    print(arr[3]["name"])
+                          
